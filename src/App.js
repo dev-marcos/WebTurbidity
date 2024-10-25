5704c42e65845159f5d65bda40ceabbac20c9a88
Marcos Rocha <marcosviniciusrochadasilva@gmail.com>
2024-10-24 23:45:12 -0300

    funcionando com toolpad

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { extendTheme, styled } from '@mui/material/styles';
+import HomeIcon from '@mui/icons-material/Home';
+import BuildIcon from '@mui/icons-material/Build';
+import WifiIcon from '@mui/icons-material/Wifi';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { AppProvider, Navigation, Router } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { PageContainer } from '@toolpad/core/PageContainer';
+import Grid from '@mui/material/Grid2';
+import Home from './pages/Home'; // Importando as páginas que você já criou
+import Calibration from './pages/Calibration';
+import Communication from './pages/Communication';
+import Settings from './pages/Settings';
+import { BluetoothProvider } from './context/BluetoothContext';
 
-function App() {
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Menu Principal',
+  },
+  {
+    segment: 'home',
+    title: 'Home',
+    icon: <HomeIcon />,
+  },
+  {
+    segment: 'calibration',
+    title: 'Calibração e Leitura',
+    icon: <BuildIcon />,
+  },
+  {
+    segment: 'communication',
+    title: 'Dados e Comunicação',
+    icon: <WifiIcon />,
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    segment: 'settings',
+    title: 'Configurações',
+    icon: <SettingsIcon />,
+  },
+];
+
+const demoTheme = extendTheme({
+  colorSchemes: { light: true, dark: true },
+  colorSchemeSelector: 'class',
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function useDemoRouter(initialPath) {
+  const [pathname, setPathname] = React.useState(initialPath);
+
+  const router = React.useMemo(() => {
+    return {
+      pathname,
+      searchParams: new URLSearchParams(),
+      navigate: (path) => setPathname(String(path)),
+    };
+  }, [pathname]);
+
+  return router;
+}
+
+const Skeleton = styled('div')(({ theme, height }) => ({
+  backgroundColor: theme.palette.action.hover,
+  borderRadius: theme.shape.borderRadius,
+  height,
+  content: '" "',
+}));
+
+function App(props) {
+  const { window } = props;
+
+  const router = useDemoRouter('/home');
+
+  const demoWindow = window ? window() : undefined;
+
+  const renderPageContent = () => {
+    switch (router.pathname) {
+      case '/home':
+        return <Home />;
+      case '/calibration':
+        return <Calibration />;
+      case '/communication':
+        return <Communication />;
+      case '/settings':
+        return <Settings />;
+      default:
+        return <Home />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BluetoothProvider>
+    <AppProvider
+      navigation={NAVIGATION}
+      router={router}
+      theme={demoTheme}
+      window={demoWindow}
+      branding={{
+        //logo: <img src='' />,
+        title: 'Web Turbidy'
+      }}
+    >
+      <DashboardLayout>
+        <PageContainer>
+          <Grid container spacing={1}>
+            <Grid size={12}>
+              {renderPageContent()}
+            </Grid>
+          </Grid>
+        </PageContainer>
+      </DashboardLayout>
+    </AppProvider>
+    </BluetoothProvider>
   );
 }
 
